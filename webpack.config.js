c6fce6897f95dd8d15e4990a7d85e03902ce88b8
Evgeny Shurupov <bolteg86@ya.ru>
2020-08-08 00:52:16 +0400

    Installed webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    devServer: {
+        contentBase: "./dist",
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
