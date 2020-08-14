4fcb747ec4afe2bed3deae52e206b57801378143
Evgeny Shurupov <bolteg86@ya.ru>
2020-08-15 02:38:06 +0400

    Added ControlsForm the first version

---
+++ src/compoents/ControlsForm/ControlsForm.stories.tsx
@@ -0,0 +1,24 @@
+import {number, withKnobs} from "@storybook/addon-knobs";
+import {ControlsForm, ControlsFormProps} from "./ControlsForm";
+import * as React from "react";
+
+export default {
+    title: "ControlsForm",
+    component: ControlsForm,
+    decorators: [withKnobs],
+};
+
+export const controlsForm = () => {
+    const settings: ControlsFormProps = {
+        changeSetting: (fieldName: string, value: number) => { console.log("fieldName", fieldName, "value", value); },
+        update: () => { console.log("Updated") },
+        alivePercent: number("alivePercent", 30),
+        animationDelay: 0,
+        animationStepsCount: 0,
+        cellSize: number("size", 10),
+        fieldHeight: number("fieldHeight", 25),
+        fieldWidth: number("fieldWidth", 25)
+    };
+
+    return<ControlsForm {...settings} />;
+};
---
+++ src/compoents/ControlsForm/ControlsForm.tsx
@@ -0,0 +1,86 @@
+import {MouseEventHandler} from "react";
+import {ConwaySettings} from "../ConwayLife/ConwayLife";
+import React from "react";
+
+export interface ControlsFormProps extends ConwaySettings {
+    changeSetting: Function;
+    update: MouseEventHandler;
+}
+
+export class ControlsForm extends React.Component<ControlsFormProps> {
+    handleChange = (fieldName: string) => (event: React.FormEvent) => {
+        const target = event.target as HTMLFormElement;
+        if (target.value === "") {
+            return;
+        }
+        const value: number = parseFloat(target.value);
+        this.props.changeSetting(fieldName, value);
+    };
+
+    render() {
+        return (
+            <form
+                style={{
+                    clear: "both",
+                }}
+            >
+                <br />
+                <label>
+                    По горизонтали:
+                    <input
+                        type="number"
+                        value={this.props.fieldWidth.toString()}
+                        onChange={this.handleChange("fieldWidth")}
+                    />
+                </label>
+                <br />
+                <label>
+                    По вертикали:
+                    <input
+                        type="number"
+                        value={this.props.fieldHeight.toString()}
+                        onChange={this.handleChange("fieldHeight")}
+                    />
+                </label>
+                <br />
+                <label>
+                    Размер клетки:
+                    <input
+                        type="number"
+                        value={this.props.cellSize.toString()}
+                        onChange={this.handleChange("cellSize")}
+                    />
+                </label>
+                <br />
+                <label>
+                    Задержка анимации:
+                    <input
+                        type="number"
+                        value={this.props.animationDelay.toString()}
+                        onChange={this.handleChange("animationDelay")}
+                    />
+                </label>
+                <br />
+                <label>
+                    Процент живых клеток:
+                    <input
+                        type="number"
+                        value={this.props.alivePercent.toString()}
+                        onChange={this.handleChange("alivePercent")}
+                    />
+                </label>
+                <br />
+                <label>
+                    Количество шагов анимации:
+                    <input
+                        type="number"
+                        value={this.props.animationStepsCount.toString()}
+                        onChange={this.handleChange("animationStepsCount")}
+                    />
+                </label>
+                <br />
+                <input type="button" value="Обновить" onClick={this.props.update} />
+            </form>
+        );
+    }
+}
